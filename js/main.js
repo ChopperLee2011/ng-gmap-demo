a15bf5014f31dfea51a69f25fdf881e30b2d742a
chopperlee <lihengpro@gmail.com>
2014-12-22 22:00:07 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('gMap', ['uiGmapgoogle-maps'])
+    .controller('mapCtrl', function($scope, $log) {
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
+        $scope.initMarkers = [];
+        $scope.dataInit = function() {
+            $log.info('init data');
+            var markers = [];
+            for (var i = 0; i < 10; i++) {
+                var marker = {
+                    latitude: $scope.map.center.latitude + (Math.random() * 2 - 1) * 1,
+                    longitude: $scope.map.center.longitude + (Math.random() * 2 - 1) * 1,
+                    id: 'm' + i,
+                    icon: 'icon/parking.png'
+                };
+                markers.push(marker);
+            }
+            $scope.initMarkers = markers;
+        };
+    });
